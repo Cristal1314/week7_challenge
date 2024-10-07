e80a9a9e517fcd9152a53e9edb910309d9bcad2d
Jimenez <CFJimenez@cps.k12.il.us>
2024-10-07 09:32:45 -0500

    first commit

---
+++ challenge_day3_python.py
@@ -0,0 +1,9 @@
+#Step 1
+text1 =input("Enter text:").lower()
+text2 = input("Three random letters?:").lower()
+
+#Step 2
+mylist=[text1]
+letter_count = mylist.count(text2)
+print(mylist.index)()
+text1.find{0}
